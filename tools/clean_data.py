c7831038274bb3cd070b8dc04b77e995a0ccfa24
Mirali Ahmadli <miraliahmadli@gmail.com>
2021-07-13 15:14:24 +0900

    Fix bugs and automate cleaning c3d markers

--- tools/clean_data.py
+++ tools/clean_data.py
@@ -1,12 +1,16 @@
 import os
 import ezc3d
+from tqdm import tqdm
 from random import randint
 
+c3d_dir = "../subjects/"
+c3d_folders = os.listdir(c3d_dir)
+two_subject_folders = ['18', '19', '20', '21', '22', '23', '33', '34']
 
 two_subjects = [('18', '19'), ('20', '21'), ('22', '23'), ('33', '34')]
 subject_names =\
         [("Justin", 'rory'), ("Justin1", 'rory1'), 
-        ("justin", "Rory"), ("Justin", "TwoSubject")]
+        ("justin", "Rory"), ("Justin", "TwoSubject1")]
 main_labels =\
         ['C7', 'CLAV', 'LANK', 'LBHD', 'LBWT', 'LELB', 'LFHD', 'LFIN', 
         'LFRM', 'LFWT', 'LHEE', 'LKNE', 'LMT5', 'LSHN', 'LSHO', 'LTHI', 
@@ -34,8 +38,10 @@ def remove_one_subject(c3d_files, subjects, out_paths=[], save=True):
     # cleanup first file
     if "LABELS2" in c1['parameters']['POINT'].keys():
         c1['parameters']['POINT']['LABELS']['value'] += c1['parameters']['POINT']['LABELS2']['value']
+        c1['parameters']['POINT']['LABELS2']['value'] = []
     if "LABELS3" in c1['parameters']['POINT'].keys():
         c1['parameters']['POINT']['LABELS']['value'] += c1['parameters']['POINT']['LABELS3']['value']
+        c1['parameters']['POINT']['LABELS3']['value'] = []
     labels1 = c1['parameters']['POINT']['LABELS']['value']
 
     pred1 = [True for _ in range(len(labels1))]
@@ -54,8 +60,10 @@ def remove_one_subject(c3d_files, subjects, out_paths=[], save=True):
     # cleanup second file
     if "LABELS2" in c2['parameters']['POINT'].keys():
         c2['parameters']['POINT']['LABELS']['value'] += c2['parameters']['POINT']['LABELS2']['value']
+        c2['parameters']['POINT']['LABELS2']['value'] = []
     if "LABELS3" in c2['parameters']['POINT'].keys():
         c2['parameters']['POINT']['LABELS']['value'] += c2['parameters']['POINT']['LABELS3']['value']
+        c2['parameters']['POINT']['LABELS3']['value'] = []
     labels2 = c2['parameters']['POINT']['LABELS']['value']
 
     pred2 = [True for _ in range(len(labels2))]
@@ -70,15 +78,10 @@ def remove_one_subject(c3d_files, subjects, out_paths=[], save=True):
         c2['parameters']['POINT']['LABELS']['value'].remove(l)
     c2['data']['points'] = c2['data']['points'][:, pred2, :]
 
-    del c1['data']['meta_points']
-    del c2['data']['meta_points']
-
     if save:
         # Write the data
-        c1.write(out_paths[0])
-        c2.write(out_paths[1])
-
-    return c1, c2
+        clean_markers(c3d=c1, out_path=out_paths[0])
+        clean_markers(c3d=c2, out_path=out_paths[1])
 
 
 def clean_markers(c3d_file="", c3d=None, out_path="", save=True):
@@ -89,8 +92,10 @@ def clean_markers(c3d_file="", c3d=None, out_path="", save=True):
 
     if "LABELS2" in c['parameters']['POINT'].keys():
         c['parameters']['POINT']['LABELS']['value'] += c['parameters']['POINT']['LABELS2']['value']
+        c['parameters']['POINT']['LABELS2']['value'] = []
     if "LABELS3" in c['parameters']['POINT'].keys():
         c['parameters']['POINT']['LABELS']['value'] += c['parameters']['POINT']['LABELS3']['value']
+        c['parameters']['POINT']['LABELS3']['value'] = []
     labels = c['parameters']['POINT']['LABELS']['value']
 
     duplicate_labels = {l:[] for l in main_labels}
@@ -133,15 +138,58 @@ def clean_markers(c3d_file="", c3d=None, out_path="", save=True):
         del c['data']['meta_points']
         c.write(out_path)
 
-    return c
+
+def clean_all(save_dir):
+    for (folder1, folder2), subjects in zip(two_subjects, subject_names):
+        path1 = os.path.join(c3d_dir, folder1)
+        path2 = os.path.join(c3d_dir, folder2)
+        c3d_files1 = [os.path.join(path1, fn) for fn in sorted(os.listdir(path1))]
+        c3d_files2 = [os.path.join(path2, fn) for fn in sorted(os.listdir(path2))]
+
+        tqdm_batch = tqdm(total=len(c3d_files1), dynamic_ncols=True)
+        for i, (fn1, fn2) in enumerate(zip(c3d_files1, c3d_files2)):
+            out_path1 = fn1.replace(c3d_dir,save_dir)
+            out_path2 = fn2.replace(c3d_dir,save_dir)
+            remove_one_subject((fn1, fn2), subjects, (out_path1, out_path2))
+
+            tqdm_update = f"iteration={i},files={fn1}, {fn2}"
+            tqdm_batch.set_postfix_str(tqdm_update)
+            tqdm_batch.update()
+        tqdm_batch.close()
+
+    for folder in c3d_folders:
+        if folder in two_subject_folders:
+            continue
+        in_path = os.path.join(c3d_dir, folder)
+        c3d_files = [os.path.join(in_path, fn) for fn in sorted(os.listdir(in_path))]
+
+        tqdm_batch = tqdm(total=len(c3d_files), dynamic_ncols=True)
+        for i, fn in enumerate(c3d_files):
+            out_path = fn.replace(c3d_dir,save_dir)
+            clean_markers(c3d_file=fn, out_path=out_path)
+
+            tqdm_update = f"iteration={i},files={fn}"
+            tqdm_batch.set_postfix_str(tqdm_update)
+            tqdm_batch.update()
+        tqdm_batch.close()
+
+
+def main(data_dir="data/"):
+    for folder in c3d_folders:
+        path = os.path.join(data_dir, folder)
+        if not os.path.exists(path):
+            os.mkdir(path)
+    clean_all(data_dir)
+
 
 if __name__ == "__main__":
-    c3d_file = "../subjects/01/01_01.c3d"
-    c3d_file = "../subjects/02/02_04.c3d"
-    c3d_files = ["../subjects/18/18_06.c3d", "../subjects/19/19_06.c3d"]
+    main()
+    # c3d_file = "../subjects/01/01_01.c3d"
+    # c3d_file = "../subjects/02/02_04.c3d"
+    # c3d_files = ["../subjects/18/18_09.c3d", "../subjects/19/19_09.c3d"]
 
     # out_path = "data/01/01_01.c3d"
-    # c = clean_markers(c3d_file, out_path)
+    # c = clean_markers(c3d_file, out_path=out_path)
 
-    out_paths = ["data/18/18_06.c3d", "data/19/19_06.c3d"]
-    c = remove_one_subject(c3d_files, subject_names[0], out_paths, save=True)
+    # out_paths = ["data/18/18_09.c3d", "data/19/19_09.c3d"]
+    # c = remove_one_subject(c3d_files, subject_names[0], out_paths, save=True)
