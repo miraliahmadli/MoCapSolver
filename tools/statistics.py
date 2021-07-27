Fix marker normalization

tools/statistics.py
@@ -17,7 +17,7 @@ def denormalize_Y(Y_hat, Y):
     return Y_denorm
 
 
-def normalize_X(X):
+def normalize_X(X_hat, X):
     '''
     Args:
         X : rotation and translation matrices
@@ -32,7 +32,7 @@ def normalize_X(X):
     X_mu = torch.mean(X, 0)
     X_std = torch.std(X, 0, unbiased=True)
 
-    X_norm = (X - X_mu.unsqueeze(0)) / X_std.unsqueeze(0)
+    X_norm = (X_hat - X_mu.unsqueeze(0)) / X_std.unsqueeze(0)
     return X_norm
 
 
@@ -55,7 +55,7 @@ def normalize_Z_pw(Z):
     return Z_norm
 
 
-def get_stat_Z(Z):
+def get_stat_Z(Z, eps=0.0001):
     '''
     Args:
         Z : local offsets
@@ -68,21 +68,20 @@ def get_stat_Z(Z):
     Z_mu = torch.mean(Z, 0)
 
     Z_ = Z - Z_mu[None, ...]
-    Z_ = Z_.view(n, -1).transpose(-1, -2)
-    Z_cov = Z_ @ Z_.transpose(-1, -2)
+    Z_ = Z_.view(n, -1)
+    Z_cov = Z_.transpose(-1, -2) @ Z_
     if n != 1:
         Z_cov /= (n-1)
 
     # Make positive sym definite
     diag = Z_cov.diagonal()
-    diag += 0.000001
+    diag += eps
 
     Z_mu = Z_mu.view(-1)
     return Z_mu, Z_cov
 
 
 def sample_Z(z_mu, z_cov, batch_size):
-    print(z_cov.shape, z_mu.shape, batch_size)
     sampler = torch.distributions.multivariate_normal.MultivariateNormal(z_mu, z_cov)
     Z = sampler.sample((batch_size, ))
     return Z
@@ -94,24 +93,26 @@ def is_psd(mat):
 
 def test_stats():
     z = np.random.rand(128, 41, 31, 3)
-    # z_cov_np = np.cov(z.reshape((100, -1)).transpose(1, 0), bias=True)
-    # z_cov_np = torch.tensor(z_cov_np)
-    # print(is_psd(z_cov_np))
+    z_cov_np = np.cov(z.reshape((128, -1)).transpose(1, 0), bias=False)
+    z_cov_np = torch.tensor(z_cov_np)
+    print(is_psd(z_cov_np))
+    print(z_cov_np.shape)
     z = torch.tensor(z).to("cuda")
     z_mu, z_cov = get_stat_Z(z)
     print("z_mu", z_mu.shape)
     print("z_cov", z_cov.shape)
     diag = z_cov.diagonal()
     # diag += 0.00000001
-    # print(is_psd(z_cov))
+    print(is_psd(z_cov))
     z_sample = sample_Z(z_mu, z_cov, z.shape[0])
-    print(z_sample)
+    # print(z_sample)
     print(z_sample.shape)
+    
 
-    # z_cov_np = torch.tensor(z_cov_np)
-    # diff = torch.abs(z_cov - z_cov_np).sum().data
-    # print(torch.max(torch.abs(z_cov_np - z_cov)))
-    # print(diff)
+    z_cov_np = torch.tensor(z_cov_np, device="cuda")
+    diff = torch.abs(z_cov - z_cov_np).sum().data
+    print(torch.max(torch.abs(z_cov_np - z_cov)))
+    print(diff)
 
 
 if __name__ == "__main__":

tools/utils.py
@@ -160,17 +160,17 @@ def svd_rot_torch(P, Q):
     assert P.shape[-2:] == Q.shape[-2:]
     d, n = P.shape[-2:]
 
-    # X,Y are n x k
+    # X,Y are d x n
     P_ = torch.sum(P, axis=-1) / n
     Q_ = torch.sum(Q, axis=-1) / n
     X = P - P_[..., None]
     Y = Q - Q_[..., None]
     Yt = Y.permute(0, 2, 1)
 
-    # S is n x n
+    # S is d x d
     S = torch.matmul(X, Yt)
 
-    # U, V are n x m
+    # U, V are d x d
     U, _, V = torch.svd(S)
     # V = V_t.permute(0, 2, 1)
     Ut = U.permute(0, 2, 1)
@@ -178,10 +178,10 @@ def svd_rot_torch(P, Q):
     det = torch.det(torch.matmul(V, Ut))
     Ut[:, -1, :] *= det.view(-1, 1)
 
-    # R is n x n
+    # R is d x d
     R = torch.matmul(V, Ut)
 
-    # t is n x k
+    # t is d x n
     t = Q_.view(-1, d, 1) - torch.matmul(R, P_.view(-1, d, 1))
 
     return R, t

train.py
@@ -51,6 +51,8 @@ class Agent:
             self.model = None
         else:
             raise NotImplementedError
+        # if torch.cuda.device_count() > 1:
+        #     self.model = nn.DataParallel(self.model)
         self.model.to(self.device)
 
     def load_data(self):
@@ -62,10 +64,15 @@ class Agent:
         self.train_steps = len(self.train_dataset) // self.cfg.batch_size
         self.val_steps = len(self.val_dataset) // self.cfg.batch_size
 
+        # train_sampler = torch.utils.data.distributed.DistributedSampler(
+        #         self.train_dataset, num_replicas=torch.cuda.device_count() )
+        # val_sampler = torch.utils.data.distributed.DistributedSampler(
+        #         self.val_dataset, shuffle=False, num_replicas=torch.cuda.device_count())
+
         self.train_data_loader = DataLoader(self.train_dataset, batch_size=self.batch_size,\
-                                            shuffle=True, num_workers=8)
+                                            shuffle=True, num_workers=8, pin_memory=True)#, sampler=train_sampler)
         self.val_data_loader = DataLoader(self.val_dataset, batch_size=self.batch_size,\
-                                            shuffle=False, num_workers=8)
+                                            shuffle=False, num_workers=8, pin_memory=True)#, sampler=val_sampler)
 
     def train(self):
         self.best_loss = float("inf")
@@ -121,14 +128,14 @@ class Agent:
             Y, Z = Y.to(torch.float32), Z.to(torch.float32)
             Y, Z = Y.to(self.device), Z.to(self.device)
 
-            # z_mu, z_cov = get_stat_Z(Z)
-            # Z_sample = sample_Z(z_mu, z_cov, self.batch_size).view(-1, self.num_markers, self.num_joints, 3)
+            z_mu, z_cov = get_stat_Z(Z)
+            Z_sample = sample_Z(z_mu, z_cov, self.batch_size).view(-1, self.num_markers, self.num_joints, 3)
 
             X = LBS(self.w, Y, Z)
-            X = corrupt(X)
+            X_hat = corrupt(X)
             Z_pw = preweighted_Z(self.w, Z)
 
-            X = normalize_X(X)
+            X = normalize_X(X_hat, X)
             Z = normalize_Z_pw(Z_pw)
 
             self.optimizer.zero_grad()
@@ -164,14 +171,14 @@ class Agent:
                 n += bs
                 Y, Z = Y.to(torch.float32).to(self.device), Z.to(torch.float32).to(self.device)
 
-                # z_mu, z_cov = get_stat_Z(Z)
-                # Z_sample = sample_Z(z_mu, z_cov, self.batch_size).view(-1, self.num_markers, self.num_joints, 3)
+                z_mu, z_cov = get_stat_Z(Z)
+                Z_sample = sample_Z(z_mu, z_cov, self.batch_size).view(-1, self.num_markers, self.num_joints, 3)
 
                 X = LBS(self.w, Y, Z)
-                X = corrupt(X)
+                X_hat = corrupt(X)
                 Z_pw = preweighted_Z(self.w, Z)
 
-                X = normalize_X(X)
+                X = normalize_X(X_hat, X)
                 Z = normalize_Z_pw(Z_pw)
 
                 Y_hat = self.model(X, Z).view(bs, self.num_joints, 3, 4)
@@ -216,14 +223,14 @@ class Agent:
             Y, Z, F = Y.to(torch.float32).to(self.device).squeeze(0), Z.to(torch.float32).to(self.device).squeeze(0), \
                         F.to(torch.float32).to(self.device).squeeze(0).unsqueeze(1)
 
-            # z_mu, z_cov = get_stat_Z(Z)
-            # Z_sample = sample_Z(z_mu, z_cov, self.batch_size).view(-1, self.num_markers, self.num_joints, 3)
+            z_mu, z_cov = get_stat_Z(Z)
+            Z_sample = sample_Z(z_mu, z_cov, self.batch_size).view(-1, self.num_markers, self.num_joints, 3)
 
             X = LBS(self.w, Y, Z)
-            X = corrupt(X)
+            X_hat = corrupt(X)
             Z_pw = preweighted_Z(self.w, Z)
 
-            X = normalize_X(X)
+            X = normalize_X(X_hat, X)
             Z = normalize_Z_pw(Z_pw)
 
             Y_hat = self.model(X, Z).view(bs, self.num_joints, 3, 4)