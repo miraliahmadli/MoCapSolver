f72e308a54b85b7cfaaf79cc94e9c0de0b822c02
beyaldiz <beyaldiz17@gmail.com>
2021-08-12 10:28:12 +0900

    Change to bvh

--- dataset/create_meta.py
+++ dataset/create_meta.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import pandas as pd
 import ezc3d
+import math
 
 
 exclude = ['103_05', '93_05', '87_02', '89_04', '89_05', # Issues with the number of markers 
@@ -26,31 +27,34 @@ def get_fps(c3d_file):
 def main():
     csv_filename = 'meta_data.csv'
     BASE_DIR = Path('.') / 'dataset'
-    df = pd.DataFrame({'amc_path': list(BASE_DIR.glob('all_asfamc/subjects/*/*.amc'))})
-    asf_files = list(BASE_DIR.glob('all_asfamc/subjects/*/*.asf'))
+    df = pd.DataFrame({'bvh_path': list(BASE_DIR.glob('all_bvh/subjects/*/*.bvh'))})
+    bvh_files = list(BASE_DIR.glob('all_bvh/subjects/*/*.bvh'))
     avg_bone = {}
-    for file in asf_files:
+    for file in bvh_files:
         avg_single = 0.0
         with open(file, 'r') as content_file:
-            content = content_file.read().split('length')
-            for i in range(len(content)):
-                splitted = content[i].split()
-                if i > 1:
-                    avg_single += float(splitted[0])
-            avg_single /= (len(content) - 2)
+            content = content_file.read().split()
+            idx = [i for i, x in enumerate(content) if x == "OFFSET"]
+            n = 0
+            for i in idx:
+                x, y, z = float(content[i + 1]), float(content[i + 2]), float(content[i + 3])
+                if x == 0 and y == 0 and z == 0:
+                    continue
+                n += 1
+                avg_single += math.sqrt(x ** 2 + y ** 2 + z ** 2)
+            avg_single /= n
         avg_bone[file] = avg_single
 
-    df['asf_path'] = df['amc_path'].map(lambda x: x.parent / (x.parent.stem + '.asf'))
-    df['c3d_path'] = df['amc_path'].map(lambda x: BASE_DIR / 'clean_c3d' / 'subjects' / x.parent.stem / (x.stem + '.c3d'))
+    df['c3d_path'] = df['bvh_path'].map(lambda x: BASE_DIR / 'clean_c3d' / 'subjects' / x.parent.stem / (x.stem + '.c3d'))
     df.drop(df[df['c3d_path'].map(lambda x: not x.exists())].index, inplace = True)
     df.drop(df[df['c3d_path'].map(lambda x: x.stem in exclude)].index, inplace = True)
-    df['motion_npy_path'] = df['amc_path'].map(lambda x: BASE_DIR / 'asfamc_npy' / 'subjects' / x.parent.stem / (x.stem + '.npy'))
+    df['motion_npy_path'] = df['bvh_path'].map(lambda x: BASE_DIR / 'bvh_npy' / 'subjects' / x.parent.stem / (x.stem + '.npy'))
     df['marker_npy_path'] = df['c3d_path'].map(lambda x: BASE_DIR / 'c3d_npy' / 'subjects' / x.parent.stem / (x.stem + '.npy'))
-    df['subject'] = df['amc_path'].map(lambda x: x.parent.stem)
-    df['activity'] = df['amc_path'].map(lambda x: x.stem.split('_')[-1].lower())
-    df['file_stem'] = df['amc_path'].map(lambda x: x.stem)
+    df['subject'] = df['bvh_path'].map(lambda x: x.parent.stem)
+    df['activity'] = df['bvh_path'].map(lambda x: x.stem.split('_')[-1].lower())
+    df['file_stem'] = df['bvh_path'].map(lambda x: x.stem)
     df['frame_rate'] = df['c3d_path'].map(lambda x: get_fps(x))
-    df['avg_bone'] = df['asf_path'].map(lambda x: avg_bone[x])
+    df['avg_bone'] = df['bvh_path'].map(lambda x: avg_bone[x])
 
     df.to_csv(BASE_DIR / csv_filename)
 
