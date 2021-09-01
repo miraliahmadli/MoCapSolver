22f80d089e3f2fa995d93cc5e1ca314552624ca2
Mirali Ahmadli <miraliahmadli@gmail.com>
2021-09-01 21:44:20 +0900

    Add some explanation to kinematics code

--- models/mocap_solver/kinematics.py
+++ models/mocap_solver/kinematics.py
@@ -11,67 +11,88 @@ import numpy as np
 
 
 class ForwardKinematics:
-    def __init__(self, edges, fk_world, pos_repr, rotation='quaternion'):
-        self.topology = [-1] * (len(edges) + 1)
-        self.rotation_map = []
-        for i, edge in enumerate(edges):
-            self.topology[edge[1]] = edge[0]
-            self.rotation_map.append(edge[1])
-
+    def __init__(self, topology, edges, fk_world=False, pos_repr="3d", rotation='quaternion'):
+        self.topology = topology # J
+        self.rotation_map = [e[1] for e in edges] # J-1, all joints except root
         self.world = fk_world
         self.pos_repr = pos_repr
         self.quater = rotation == 'quaternion'
 
-    def forward_from_raw(self, raw, offset, world=None, quater=None):
-        if world is None: world = self.world
-        if quater is None: quater = self.quater
+    def process_raw_repr(self, raw, quater=None):
         if self.pos_repr == '3d':
-            position = raw[:, -3:, :]
-            rotation = raw[:, :-3, :]
+            # raw: bs x ((J-1) * 4 + 3) x T
+            position = raw[:, -3:, :] # bs x 3 x T
+            rotation = raw[:, :-3, :] # bs x ((J-1) * 4) x T
         elif self.pos_repr == '4d':
             raise Exception('Not support')
+
         if quater:
-            rotation = rotation.reshape((rotation.shape[0], -1, 4, rotation.shape[-1]))
+            rotation = rotation.reshape((rotation.shape[0], -1, 4, rotation.shape[-1])) # bs x (J-1) x 4 x T
             identity = torch.tensor((1, 0, 0, 0), dtype=torch.float, device=raw.device)
         else:
             rotation = rotation.reshape((rotation.shape[0], -1, 3, rotation.shape[-1]))
             identity = torch.zeros((3, ), dtype=torch.float, device=raw.device)
-        identity = identity.reshape((1, 1, -1, 1))
+        
+        identity = identity.view((1, 1, -1, 1))
         new_shape = list(rotation.shape)
         new_shape[1] += 1
         new_shape[2] = 1
-        rotation_final = identity.repeat(new_shape)
+        rotation_final = identity.repeat(new_shape) # bs x J x 3/4 x T
+
+        # put joint's rotation in corresponding position in its mapping
         for i, j in enumerate(self.rotation_map):
             rotation_final[:, j, :, :] = rotation[:, i, :, :]
-        return self.forward(rotation_final, position, offset, world=world, quater=quater)
+        
+        return rotation_final, position, identity
 
-    '''
-    rotation should have shape batch_size * Joint_num * (3/4) * Time
-    position should have shape batch_size * 3 * Time
-    offset should have shape batch_size * Joint_num * 3
-    output have shape batch_size * Time * Joint_num * 3
-    '''
+    def forward_from_raw(self, raw, offset, world=None, quater=None):
+        '''
+        Separates raw rotation matrix into rotation matrices of joints and global position of root
+        and then applies forward kinematics
+
+        Input:
+            raw should have shape batch_size * (Joint_num - 1) * 3/4 + 3) * Time
+            offset should have shape batch_size * Joint_num * 3
+        Output:
+            rotation_final have shape batch_size * Joint_num * (3/4) * Time
+            we then apply forward kinematics
+        '''
+        if world is None: world = self.world
+        if quater is None: quater = self.quater
+
+        rotation, position, identity = self.process_raw_repr(raw, quater)
+
+        return self.forward(rotation, position, offset, world=world, quater=quater)
+
     def forward(self, rotation: torch.Tensor, position: torch.Tensor, offset: torch.Tensor, order='xyz', quater=False, world=True):
+        '''
+        Computes local or global positions of each joint (depending on world argument)
+
+        Input:
+            rotation should have shape batch_size * Joint_num * (3/4) * Time
+            position should have shape batch_size * 3 * Time
+            offset should have shape batch_size * Joint_num * 3
+        Output:
+            output have shape batch_size * Time * Joint_num * 3
+        '''
         if not quater and rotation.shape[-2] != 3: raise Exception('Unexpected shape of rotation')
         if quater and rotation.shape[-2] != 4: raise Exception('Unexpected shape of rotation')
-        rotation = rotation.permute(0, 3, 1, 2)
-        position = position.permute(0, 2, 1)
-        result = torch.empty(rotation.shape[:-1] + (3, ), device=position.device)
+        rotation = rotation.permute(0, 3, 1, 2) # bs x T x J x 4
+        position = position.permute(0, 2, 1) # bs x T x 3
+        result = torch.empty(rotation.shape[:-1] + (3, ), device=position.device) # bs x T x J x 3
 
-
+        # normalize rotation matrix
         norm = torch.norm(rotation, dim=-1, keepdim=True)
-        #norm[norm < 1e-10] = 1
         rotation = rotation / norm
 
-
         if quater:
-            transform = self.transform_from_quaternion(rotation)
+            transform = self.transform_from_quaternion(rotation) # bs x T x J x 3 x 3
         else:
             transform = self.transform_from_euler(rotation, order)
 
-        offset = offset.reshape((-1, 1, offset.shape[-2], offset.shape[-1], 1))
+        offset = offset.reshape((-1, 1, offset.shape[-2], offset.shape[-1], 1)) # bs x 1 x J x 3 x 1
 
-        result[..., 0, :] = position
+        result[..., 0, :] = position # root's result will be its global position
         for i, pi in enumerate(self.topology):
             if pi == -1:
                 assert i == 0
